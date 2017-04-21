8022c4b51f8bd29ed85da0e24c4aa60ba5255c12
jukhan <jasim.uddin.khan@gmail.com>
2017-04-21 23:59:52 +0600

    animation

--- src/Animation.tsx
+++ src/Animation.tsx
@@ -23,13 +23,11 @@ export default class Animation{
                     width:'20px',
                     height:'20px',                    
                     top:(ev.clientY-70)+'px',
-                    left:(ev.clientX-80)+'px', 
+                    left:(ev.clientX-120)+'px', 
                     position: 'absolute',
-                    backgroundColor: "rgb(" + (this.random(0, 255)) + ", " + (this.random(0, 255)) + ", " + (this.random(0, 255)) + ")",
-                    // opacity: '0', 
-                    // transition: 'opacity 500ms', 
-                    // delayed: {opacity: '1'},
-                    // remove: {opacity: '0'},
+                    backgroundColor: `rgb(${this.random(0, 255)},${this.random(0, 255)},${this.random(0, 255)})`,
+                    borderRadius:'.4em',
+                    boxShadow: `-7px -8px 6px -7px rgba(${this.random(0, 255)},${this.random(0, 255)},${this.random(0, 255)},0.75)`
                 };
                 action.type='new-box'
                 return Observable.of(action);
@@ -42,11 +40,11 @@ export default class Animation{
     view({model, dispatch}){
         return <div  on-mousemove={ev=>Router.CM.actions$.next({type:'mousemove',payload:ev,dispatch})}
                 style={{width:'100%', height:'400px',border:'#ddd 1px solid'}}>
-            {model.boxList.map((box, key)=><this.Box model={box} key={key}/>)}
+            {model.boxList.map(box=><this.Box model={box}/>)}
         </div>
     }
     Box({model, key}){       
-        return <div key={key} style={model}></div>
+        return <div style={model}></div>
     }
     update(model, action){
         Router.CM.actions$.dispatch(action);
@@ -56,7 +54,7 @@ export default class Animation{
                 if(model.boxList.length>25){
                     model.boxList.shift();
                 } 
-                return model;       
+                return {boxList:model.boxList};       
             default:
                 return model;
         }
