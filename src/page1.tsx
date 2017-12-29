15e5918433cc5f4f4e6f07167a7f72ed1945a07b
jasim <jasim@zitelab.dk>
2017-12-29 19:23:26 +0600

    animation

--- src/page1.tsx
+++ src/page1.tsx
@@ -1,15 +1,64 @@
-
+import { ViewObj, Router } from 'zaitun';
 import { html } from 'snabbdom-jsx';
 
-function view() {
-    return <div 
-        classNames="dx"        
-        hook-insert={vnode => console.log(vnode.elm.getBoundingClientRect())}
-        hook-destroy={vnode => console.log(vnode.elm)}
-        >
-        <h3 classNames="blue-background">Page-1</h3>
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/of';
+import 'rxjs/add/operator/mergeMap';
+import 'rxjs/add/operator/delay';
 
-    </div>;
+function init() {
+    return { boxList: [] }
+}
+function afterViewRender(dispatch, router: Router) {
+    router.effect$
+        .addEffect(eff =>
+            eff.whenAction('mousemove')
+                .delay(0)
+                .mergeMap(action => {
+                    const ev = action.payload;
+                    action.payload = {
+                        key: Math.floor(Math.random() * 1000000),
+                        style: {
+                            opacity: '0', transition: 'opacity 1s',
+                            delayed: { opacity: '1' },
+                            remove: { opacity: '0' },
+                            width: '20px',
+                            height: '20px',
+                            top: (ev.clientY - 7) + 'px',
+                            left: (ev.clientX - 12) + 'px',
+                            position: 'absolute',
+                            backgroundColor: `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`,
+                            borderRadius: '.4em',
+                            boxShadow: `-7px -8px 6px -7px rgba(${random(0, 255)},${random(0, 255)},${random(0, 255)},0.75)`
+                        }
+                    };
+                    action.type = 'new-box'
+                    return Observable.of(action);
+                })
+        );
+}
+function random(low, high) {
+    return Math.floor(Math.random() * (high - low + 1)) + low;
+}
+function view({ model, dispatch }: ViewObj) {
+    return <div on-mousemove={ev => dispatch({ type: 'mousemove', payload: ev }, true)}
+        style={{ width: '100%', height: '400px', border: '#ddd 1px solid' }}>
+        {model.boxList.map(box => <div key={box.key} style={box.style}></div>)}
+    </div>
 }
 
-export default { view };
+function update(model, action) {
+    switch (action.type) {
+        case 'new-box':
+            model.boxList.push(action.payload);
+            if (model.boxList.length > 25) {
+                model.boxList.shift();
+            }
+            return { boxList: [...model.boxList] };
+        default:
+            return model;
+    }
+}
+
+
+export default { view, update, init, afterViewRender };
